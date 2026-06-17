fb55aeceefc15168d8e242b31bdda5be71253862
mihin <huguesaime.mihin@gmail.com>
2026-06-17 17:33:36 +0000

    seed agerange

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             EducationLevelSeeder::class,
             PermissionSeeder::class,
+            AgeRangeSeeder::class
         ]);
 
         User::firstOrCreate(
